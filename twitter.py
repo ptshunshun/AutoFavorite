314ce8f505cacf925c0b7d6979d99f2aeed97a85
中村駿介 <shun@nakamurashunsukenoMacBook-Pro.local>
2020-04-15 08:38:54 +0900

    add Procfile requirements runtime

--- twitter.py
+++ twitter.py
@@ -12,15 +12,21 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
 api = tweepy.API(auth)
 
-
+dic = {}  # 辞書を定義しておきます。
 q_list = ["プログラミング", "デイサービス"]
-count=10
+count = 10
 
 for q in q_list:
     search_results = api.search(q=q, count=count)
     for result in search_results:
-        tweet_id = result.id
-        try:
-            api.create_favorite(tweet_id)
-        except Exception as e:
-            print(e)
+        if result.retweeted == False:
+            text = result.text  # ツイートのテキスト部分を変数textに代入。
+            id = result.id  # ツイートの識別番号を変数idに代入
+            dic.update({id: text})  # 変数textと変数idの紐付けをし、辞書dicに追加。
+            if '@' in dic[id]:  # 辞書dicにキーである変数idを代入し、テキスト本文を出力。＠を含むツイート（リプライ）を除外。
+                pass
+            else:
+                api.create_favorite(id)
+
+            # except Exception as e:
+                # print(e)
